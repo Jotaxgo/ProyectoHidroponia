5795a709e72a68fa2c262ad6ab244a76f59e9d2f
Jotaxgo <juampi94.jpcc@gmail.com>
2025-11-16 21:53:27 -0400

    Se soluciono errores de sesion

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -25,6 +26,10 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (TokenMismatchException $e, $request) {
+            return redirect()->route('login')->with('status', 'Tu sesión ha expirado. Por favor, inicia sesión de nuevo.');
         });
     }
 }
